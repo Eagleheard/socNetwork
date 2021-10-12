d2505803baaf2a39b2b5c5c75101f6f723a5cfe0
Vladislav Krutikov <Eagleheard5@gmail.com>
2021-10-12 13:01:47 +0300

    changed person photo

--- src/components/Profile/PersonInfo/PersonInfo.jsx
+++ src/components/Profile/PersonInfo/PersonInfo.jsx
@@ -11,7 +11,7 @@ const PersonInfo = (props) => {
                 <img src='https://c4.wallpaperflare.com/wallpaper/135/681/249/field-flowers-hd-cornflower-field-wallpaper-preview.jpg' className={s.backlay} />
             </div>
             <div className={s.ava}>
-                <img src={props.profile.photos.small} className={s.persava} />
+                <img src={props.profile.photos.small !=null ? props.profile.photos.small :  'https://sun2.beltelecom-by-minsk.userapi.com/s/v1/ig2/eQaVr0W4vwoLViiso_ZjIuNnemsFCgBkImSta2yM6NTq2aDBkZaW_FZT4tMG5g1wE268Hoj7ztoCnN-Ch4UFarv2.jpg?size=200x224&quality=96&crop=182,233,726,816&ava=1'} className={s.persava}/>
             </div>
             <div className={s.desc}>
                <span>{props.profile.aboutMe}</span>
